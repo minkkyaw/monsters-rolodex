3e63b7c72d6896d3cd42f50e60de1b9a379cfee2
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-08-08 18:21:24 -0400

    Updates

---
+++ precache-manifest.aceec16720db4dddfc0c26432ed1fa15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b4d027293c634aacb13a980a0b0d69",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec4428e0e8f8e00d21f9",
+    "url": "/monsters-rolodex/static/css/main.290c88ab.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ec4428e0e8f8e00d21f9",
+    "url": "/monsters-rolodex/static/js/main.d2c582fc.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
